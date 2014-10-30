144020f5e0f31a763e3ce1d5b9422ccf943d86fd
Ge0rges <giokanaan@gmail.com>
2014-10-30 18:46:21 +0200

    Initial Commit

---
+++ Bloard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bloard
+//
+//  Created by Georges Kanaan on 10/30/14.
+//  Copyright (c) 2014 Georges Kanaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
